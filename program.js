affd87744b6a7972f48a7e652417737fa75476b5
MaxWofford <max.speed.wofford@gmail.com>
2014-07-31 20:47:33 -0700

    Finished exercise 4

--- program.js
+++ program.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
-
-var cake = process.argv[2];
-var pie = fs.readFileSync(cake);
-var str = pie.toString();
-var splits = str.split('\n');
-// console.log(splits)
-console.log(splits.length - 1);
+var pathToFile = process.argv[2];
+var bufferString, bufferStringSplit;
+	fs.readFile(pathToFile, function (err, data) {
+		bufferStringLength = data.toString().split('\n').length - 1;
+		console.log(bufferStringLength)
+	});
